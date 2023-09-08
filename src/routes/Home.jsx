2f1206a39c65bd5440e01b16778e2b9c5aabd49e
IceManbrz <81310383+IceManbrz@users.noreply.github.com>
2023-09-09 00:11:00 +0700

    change ui

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,36 +3,39 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 const apiKey = process.env.REACT_APP_API_HOST;
 const Home = () =>{
-    const [name, setName] = useState([]);
-    const [loading, setLoading] = useState(false);
-
-    console.log(apiKey);
-
-useEffect(() =>{
-  setLoading(false);
-    fetch (apiKey)
-    .then((response) => response.json()) 
-  //.then((json) => console.log(json))
-  .then((json) => setName((json)));
- //JSON.stringify
-}, []);
-return(
-    <div>
-<div className="bg-green-500 pt-20 pb-20">
-  
-<h1 className="ml-10">test123</h1>
-</div>
-<div className="container mx-auto p-4">
-<div class="grid grid-cols-3 grid-rows-2 gap-4 pt-4">
-  {
-    
-  }
-  
-</div>
-</div>
-</div>
-)
-
+  return (
+    <div className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 min-h-screen flex flex-col items-center justify-center text-white">
+      <header className="text-center">
+        <h1 className="text-4xl font-extrabold mb-4">Selamat Datang di Gymonline</h1>
+        <p className="text-xl mb-8">Platform kami memberikan manuk.</p>
+        <a
+          href="/signup" // Ganti dengan URL tautan ke halaman pendaftaran
+          className="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold px-6 py-3 rounded-lg transition duration-300 transform hover:scale-105">
+          Daftar Sekarang
+        </a>
+      </header>
+      <section className="mt-12">
+        <h2 className="text-3xl font-extrabold mb-4">Fitur </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <h3 className="text-xl font-semibold mb-2">Fitur 1</h3>
+            <p className="text-gray-600">Deskripsi fitur 1 yang menarik.</p>
+          </div>
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <h3 className="text-xl font-semibold mb-2">Fitur 2</h3>
+            <p className="text-gray-600">Deskripsi fitur 2 yang menarik.</p>
+          </div>
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <h3 className="text-xl font-semibold mb-2">Fitur 3</h3>
+            <p className="text-gray-600">Deskripsi fitur 3 yang menarik.</p>
+          </div>
+        </div>
+      </section>
+      <footer className="mt-12 text-center text-gray-400">
+        &copy; 2023 Nama Perusahaan. Hak Cipta Dilindungi.
+      </footer>
+    </div>
+  );
 }
 
 
